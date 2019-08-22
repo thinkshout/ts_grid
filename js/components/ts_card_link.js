a2de42fc556d487c9977a603e0773eb1bcf73e3b
Amy VS <amy.vaillancourtsals@thinkshout.com>
2019-08-22 16:05:50 -0700

    Update ts_card_link to tend to scenarios where links are not present.

--- js/components/ts_card_link.js
+++ js/components/ts_card_link.js
@@ -5,22 +5,32 @@
  */
 
 export default function(context) {
-	// // Alias global jQuery object.
-	const $ = jQuery;
-	if (context == document) {
-		$("div[data-href]:not([data-href=\"\"])", context).each(function () {
-			$(this).click(function (e) {
-				if ($(e.target).closest('[data-contextual-id]').length) {
-					return;
-				}
-				window.location.href = $(this).attr('data-href');
-			});
-		});
-		$('div[data-href-target][data-href-trigger]', context).once('ts-grid-card-link').each(function () {
-			var $parent = $(this);
-			$parent.find($(this).attr('data-href-trigger')).on('click', function () {
-				window.location.href = $parent.find($parent.attr('data-href-target')).attr('href');
-			});
-		});
-	}
+  // // Alias global jQuery object.
+  const $ = jQuery;
+  if (context == document) {
+    $('div[data-href]:not([data-href=""])', context).each(function() {
+      $(this).click(function(e) {
+        if ($(e.target).closest("[data-contextual-id]").length) {
+          return;
+        }
+        window.location.href = $(this).attr("data-href");
+      });
+    });
+
+    $("div[data-href-target][data-href-trigger]", context)
+      .once("ts-grid-card-link")
+      .each(function() {
+        var $parent = $(this);
+        var $link = $parent.find($(this).attr("data-href-target"));
+        var $trigger = $parent.find($(this).attr("data-href-trigger"));
+
+        if ($link.length > 0) {
+          $trigger.on("click", function() {
+            window.location.href = $parent
+              .find($parent.attr("data-href-target"))
+              .attr("href");
+          });
+        }
+      });
+  }
 }
